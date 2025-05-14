4fde5d570d493dbe7d5a30ba7750b9fd75693809
torugoProgrammer <vizombr@gmail.com>
2025-05-14 20:31:20 -0300

    teste

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session
 import os
 from dotenv import load_dotenv
 import pymysql
 from routes import user_router
+
 pymysql.install_as_MySQLdb()
 
 load_dotenv()
@@ -14,7 +15,7 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(os.getenv('DB_URL'))
+engine = create_engine(DB_URL)
     
 app = FastAPI()
 
@@ -26,4 +27,9 @@ app.include_router(user_router.router)
 
 @app.get('/')
 def test():
-    return 'hello'
+    with Session() as session:
+        user = {"id": 0, "name": "dorugo", "cpf": "12312312398"}
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
